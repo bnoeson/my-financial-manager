dd788f7d93ae08f6112e4642aeab3f07fbbe5f8c
bnoeson <boris_noeson@hotmail.com>
2018-05-11 15:46:03 +0200

    initial commit

---
+++ src/main/java/be/noeson/myfinancialmanager/MyFinancialManagerApplication.java
@@ -0,0 +1,12 @@
+package be.noeson.myfinancialmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinancialManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinancialManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/be/noeson/myfinancialmanager/MyfinancialmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.noeson.myfinancialmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyfinancialmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
